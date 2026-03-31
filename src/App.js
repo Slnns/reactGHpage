422d022d329711eac8f92c69b41f2f6973d6c3d4
Slnns <lenaperchak@gmail.com>
2026-03-31 08:45:01 +0400

    add gh actions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
+import { HashRouter, Routes, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import App2 from './App2';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World</h1>
-        <p>
-          Hello Hello Hello Hello Hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          
+          <nav>
+            <Link to="/" style={{ color: 'white', margin: '10px' }}>Home</Link>
+            <Link to="/app2" style={{ color: 'white', margin: '10px' }}>Page</Link>
+          </nav>
+
+          <Routes>
+            <Route path="/" element={<h1>Hello World</h1>} />
+            <Route path="/app2" element={<App2 />} />
+          </Routes>
+        </header>
+      </div>
+    </HashRouter>
   );
 }
 
---
+++ src/App2.js
@@ -0,0 +1,10 @@
+function App2() {
+  return (
+    <div>
+      <h1>App2 Page</h1>
+      <p>Это вторая страница</p>
+    </div>
+  );
+}
+
+export default App2;
